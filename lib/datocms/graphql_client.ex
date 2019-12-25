1f97522b6c337ce0be691d5af90ce3a711fbd889
Joe Yates <joe.g.yates@gmail.com>
2019-12-25 18:18:58 +0100

    Handle pagination by wrapping simple queries

--- lib/datocms/graphql_client.ex
+++ lib/datocms/graphql_client.ex
@@ -13,14 +13,39 @@ defmodule DatoCMS.GraphQLClient do
     Neuron.Config.set(parse_options: [keys: :atoms])
   end
 
+  def fetch_all!(query, key, options \\ []) do
+    {:ok, pages} = fetch_all(query, key, options)
+    pages
+  end
+
   def fetch_all(query, key, options \\ []) do
+    paginated = """
+      query paginated($first: IntType!, $skip: IntType!) {
+        #{key}(first: $first, skip: $skip) #{query}
+      }
+    """
     skip = options[:skip] || 0
     first = options[:first] || @per_page
-    {:ok, response} = Neuron.query(query, %{skip: skip, first: first})
-    page = response.body[:data][key]
+    do_fetch_all(paginated, key, [skip: skip, first: first])
+  end
+
+  defp do_fetch_all(paginated, key, options) do
+    skip = options[:skip]
+    first = options[:first]
+    {:ok, response} = Neuron.query(paginated, %{skip: skip, first: first})
+    handle_fetch_all_response(response.body, paginated, key, options)
+  end
+
+  defp handle_fetch_all_response(%{errors: errors} = body, _paginated, _key, _options), do: {:error, errors}
+  defp handle_fetch_all_response(body, paginated, key, options) do
+    page = body[:data][key]
     case page do
-      [] -> []
-      _  -> page ++ fetch_all(query, key, Keyword.put(options, :skip, skip + first))
+      [] -> {:ok, []}
+      _  ->
+        skip = options[:skip]
+        first = options[:first]
+        {:ok, pages} = do_fetch_all(paginated, key, Keyword.put(options, :skip, skip + first))
+        {:ok, page ++ pages}
     end
   end
 end
