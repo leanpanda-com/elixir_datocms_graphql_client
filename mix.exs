0f07673ee397ee3e475ef20a679059f22f7b0809
Joe Yates <joe.g.yates@gmail.com>
2019-12-27 15:48:15 +0100

    Bump for release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DatoCMS.GraphqlClient.MixProject do
   def project do
     [
       app: :datocms_graphql_client,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
