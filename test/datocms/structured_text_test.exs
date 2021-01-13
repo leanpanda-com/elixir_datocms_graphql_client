cbb9d5c5d2dbbc1d0510e22e6b9d4ac2c2493b31
Joe Yates <joe.g.yates@gmail.com>
2021-01-13 15:22:11 +0100

    Extract module

--- test/datocms/structured_text_test.exs
+++ test/datocms/structured_text_test.exs
@@ -1,20 +1,5 @@
-defmodule DatoCMS.StructuredTextTest do
-  use ExUnit.Case, async: true
-  import DatoCMS.Test.Support.FixtureHelper, only: [json_fixture!: 1]
-
-  import DatoCMS.StructuredText, only: [
-    to_html: 1,
-    to_html: 2,
-    render: 3
-  ]
-
-  def render_inline_record(%{__typename: "ItemRecord"} = item) do
-    "<h1>#{item.title}</h1><p>#{item.body}</p>"
-  end
-
-  def render_link_to_record(%{__typename: "ItemRecord"} = item, node) do
-    ~s(<a href="/items/#{item.id}">#{hd(node.children).value}</a>)
-  end
+defmodule DatoCMS.StructuredTextTest.CustomRenderers do
+  import DatoCMS.StructuredText, only: [render: 3]
 
   def render_custom_heading(node, dast, options) do
     tag = "h#{node.level + 1}"
@@ -33,6 +18,25 @@ defmodule DatoCMS.StructuredTextTest do
     simplified = Map.put(span, :marks, marks)
     ~s(<span class="bright>) <> render(simplified, dast, options) <> "</span>"
   end
+
+  def render_inline_record(%{__typename: "ItemRecord"} = item) do
+    "<h1>#{item.title}</h1><p>#{item.body}</p>"
+  end
+
+  def render_link_to_record(%{__typename: "ItemRecord"} = item, node) do
+    ~s(<a href="/items/#{item.id}">#{hd(node.children).value}</a>)
+  end
+end
+
+defmodule DatoCMS.StructuredTextTest do
+  use ExUnit.Case, async: true
+  import DatoCMS.Test.Support.FixtureHelper, only: [json_fixture!: 1]
+  import DatoCMS.StructuredTextTest.CustomRenderers
+
+  import DatoCMS.StructuredText, only: [
+    to_html: 1,
+    to_html: 2
+  ]
 
   @tag structured_text: json_fixture!("minimal-text")
   test "minimal text", context do
