176c59f17a91ee4d201031f65dbca414c9e0826d
Joe Yates <joe.g.yates@gmail.com>
2021-03-30 10:24:57 +0200

    Update ex_doc

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule DatoCMS.GraphqlClient.MixProject do
   defp deps do
     [
       {:eventsource_ex, "~> 1.0"},
-      {:ex_doc, "~> 0.21.2", only: :dev},
+      {:ex_doc, "~> 0.21", only: :dev},
       {:jason, ">= 0.0.0"},
       {:memoize, ">= 1.3.0"},
       {:neuron, "~> 4.1.0"}
