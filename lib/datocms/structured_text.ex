5118e3d66ac055a1113e5d80e53fd63726d1cd2c
Joe Yates <joe.g.yates@gmail.com>
2021-07-06 11:43:22 +0200

    Standardize access to renderers

--- lib/datocms/structured_text.ex
+++ lib/datocms/structured_text.ex
@@ -24,111 +24,111 @@ defmodule DatoCMS.StructuredText do
   end
 
   def render(%{type: "blockquote"} = node, dast, options) do
-    render_blockquote = get_in(options, [:renderers, :render_blockquote])
-    if render_blockquote do
-      render_blockquote.(node, dast, options) |> list()
-    else
-      caption = if Map.has_key?(node, :attribution) do
-        ["<figcaption>— #{node.attribution}</figcaption>"]
-      else
-        []
-      end
+    case renderer(options, :render_blockquote) do
+      {:ok, renderer} ->
+        renderer.(node, dast, options) |> list()
+      _ ->
+        caption = if Map.has_key?(node, :attribution) do
+            ["<figcaption>— #{node.attribution}</figcaption>"]
+          else
+            []
+          end
 
-      ["<figure>"] ++
-        ["<blockquote>"] ++
-        Enum.flat_map(node.children, &(render(&1, dast, options))) ++
-        ["</blockquote>"] ++
-        caption ++
-        ["</figure>"]
+        ["<figure>"] ++
+          ["<blockquote>"] ++
+          Enum.flat_map(node.children, &(render(&1, dast, options))) ++
+          ["</blockquote>"] ++
+          caption ++
+          ["</figure>"]
     end
   end
 
   def render(%{type: "code", code: code} = node, dast, options) do
-    render_code = get_in(options, [:renderers, :render_code])
-    if render_code do
-      render_code.(node, dast, options) |> list()
-    else
+    case renderer(options, :render_code) do
+      {:ok, renderer} ->
+        renderer.(node, dast, options) |> list()
+    _ ->
       ["<code>", code, "</code>"]
     end
   end
 
   def render(%{type: "list", style: "bulleted"} = node, dast, options) do
-    render_bulleted_list = get_in(options, [:renderers, :render_bulleted_list])
-    if render_bulleted_list do
-      render_bulleted_list.(node, dast, options) |> list()
-    else
-      ["<ul>"] ++
-        Enum.flat_map(
-          node.children,
-          fn list_item ->
-            ["<li>"] ++
-              Enum.flat_map(list_item.children, &(render(&1, dast, options))) ++
-              ["</li>"]
-          end
-        ) ++
-        ["</ul>"]
+    case renderer(options, :render_bulleted_list) do
+      {:ok, renderer} ->
+        renderer.(node, dast, options) |> list()
+      _ ->
+        ["<ul>"] ++
+          Enum.flat_map(
+            node.children,
+            fn list_item ->
+              ["<li>"] ++
+                Enum.flat_map(list_item.children, &(render(&1, dast, options))) ++
+                ["</li>"]
+            end
+          ) ++
+          ["</ul>"]
     end
   end
 
   def render(%{type: "list", style: "numbered"} = node, dast, options) do
-    render_numbered_list = get_in(options, [:renderers, :render_numbered_list])
-    if render_numbered_list do
-      render_numbered_list.(node, dast, options) |> list()
-    else
-      ["<ol>"] ++
-        Enum.flat_map(
-          node.children,
-          fn list_item ->
-            ["<li>"] ++
-              Enum.flat_map(list_item.children, &(render(&1, dast, options))) ++
-              ["</li>"]
-          end
-        ) ++
-        ["</ol>"]
+    case renderer(options, :render_numbered_list) do
+      {:ok, renderer} ->
+        renderer.(node, dast, options) |> list()
+      _ ->
+        ["<ol>"] ++
+          Enum.flat_map(
+            node.children,
+            fn list_item ->
+              ["<li>"] ++
+                Enum.flat_map(list_item.children, &(render(&1, dast, options))) ++
+                ["</li>"]
+            end
+          ) ++
+          ["</ol>"]
     end
   end
 
   def render(%{type: "paragraph"} = node, dast, options) do
-    renderers = options[:renderers] || %{}
-    if renderers[:render_paragraph] do
-      renderers[:render_paragraph].(node, dast, options) |> list()
-    else
-      inner = Enum.flat_map(node.children, &(render(&1, dast, options)))
-      ["<p>"] ++ inner ++ ["</p>"]
+    case renderer(options, :render_paragraph) do
+      {:ok, renderer} ->
+        renderer.(node, dast, options) |> list()
+      _ ->
+        inner = Enum.flat_map(node.children, &(render(&1, dast, options)))
+        ["<p>"] ++ inner ++ ["</p>"]
     end
   end
 
   def render(%{type: "heading"} = node, dast, options) do
-    renderers = options[:renderers] || %{}
-    if renderers[:render_heading] do
-      renderers[:render_heading].(node, dast, options) |> list()
-    else
-      tag = "h#{node.level}"
-      inner = Enum.flat_map(node.children, &(render(&1, dast, options)))
-      ["<#{tag}>"] ++ inner ++ ["</#{tag}>"]
+    case renderer(options, :render_heading) do
+      {:ok, renderer} ->
+        renderer.(node, dast, options) |> list()
+      _ ->
+        tag = "h#{node.level}"
+        inner = Enum.flat_map(node.children, &(render(&1, dast, options)))
+        ["<#{tag}>"] ++ inner ++ ["</#{tag}>"]
     end
   end
 
   def render(%{type: "link"} = node, dast, options) do
-    renderers = options[:renderers] || %{}
-    if renderers[:render_link] do
-      renderers[:render_link].(node, dast, options) |> list()
-    else
-      inner = Enum.flat_map(node.children, &(render(&1, dast, options)))
-      [~s(<a href="#{node.url}">)] ++ inner ++ ["</a>"]
+    case renderer(options, :render_link) do
+      {:ok, renderer} ->
+        renderer.(node, dast, options) |> list()
+      _ ->
+        inner = Enum.flat_map(node.children, &(render(&1, dast, options)))
+        [~s(<a href="#{node.url}">)] ++ inner ++ ["</a>"]
     end
   end
 
   def render(%{type: "span", marks: [mark | marks]} = node, dast, options) do
-    renderers = options[:renderers] || %{}
     renderer_key = :"render_#{mark}"
-    if renderers[renderer_key] do
-      renderers[renderer_key].(node, dast, options) |> list()
-    else
-      simplified = Map.put(node, :marks, marks)
-      inner = render(simplified, dast, options)
-      node = @mark_nodes[mark]
-      ["<#{node}>"] ++ inner ++ ["</#{node}>"]
+    case renderer(options, renderer_key) do
+      {:ok, renderer} ->
+        renderer.(node, dast, options) |> list()
+      _ ->
+        simplified = Map.put(node, :marks, marks)
+        inner = render(simplified, dast, options)
+        node = @mark_nodes[mark]
+        ["<#{node}>"] ++ inner ++ ["</#{node}>"]
     end
   end
 
